83b52a1c2272bd7dc96969a34aa014a8b8ab2a30
hansolc <hsc080808@naver.com>
2023-06-07 20:24:40 +0900

    Add Fullpage Scrolled Func

--- src/app/Homepage.tsx
+++ src/app/Homepage.tsx
@@ -1,71 +1,81 @@
 "use client";
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import FirstBanner from "./banners/FirstBanner";
 import SecondBanner from "./banners/SecondBanner";
 import ThirdBanner from "./banners/ThirdBanner";
 import styles from "./page.module.scss";
 import { throttle } from "lodash";
 
-const temp = [0, 937, 1874];
-
 export default function HomePage() {
-  const wrapperEl = useRef<HTMLDivElement>(null);
-  const [temp, setTemp] = useState<number>(0);
-  useEffect(() => {
-    window.scrollTo(0, temp);
-  }, [temp]);
+  const currentIndex = useRef<number>(0);
+  const [elemY, setElemY] = useState<number[]>([]);
 
-  useEffect(() => {
-    const io = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach((entry) => {
-          const { isIntersecting, target } = entry;
-          const { top } = entry.intersectionRect;
-          //   console.log(isIntersecting, target);
-          console.log(target);
-          if (isIntersecting) {
-            const moveY = target.getBoundingClientRect().top + window.scrollY;
-            console.log(moveY);
-            // window.scroll({
-            //   top: moveY,
-            //   behavior: "smooth",
-            // });
-            setTemp(moveY);
-          }
-        });
-      },
-      { threshold: 0.1 }
-    );
-
-    if (wrapperEl.current) {
-      const { current } = wrapperEl;
-      for (let i = 0; i < current.children.length; i++) {
-        io.observe(current.children[i]);
+  const handleScroll = useCallback(
+    (e: WheelEvent) => {
+      e.preventDefault();
+      const { current } = currentIndex;
+      if (e.deltaY > 0 && current < elemY.length - 1) {
+        //scroll down
+        console.log("down");
+        currentIndex.current += 1;
+        window.scrollTo(0, elemY[currentIndex.current]);
+      } else if (e.deltaY < 0 && current > 0) {
+        //scroll up
+        console.log("up");
+        currentIndex.current -= 1;
+        window.scrollTo(0, elemY[currentIndex.current]);
       }
+    },
+    [elemY]
+  );
+
+  useEffect(() => {
+    if (elemY.length !== 0) {
+      window.addEventListener(
+        "wheel",
+        throttle(handleScroll, 250, { leading: true, trailing: false }),
+        { passive: false }
+      );
+
+      return () => {
+        window.removeEventListener("wheel", handleScroll);
+      };
     }
-    // setTimeout(() => {
-    //   window.scrollTo(0, 937);
-    // }, 1000);
+  }, [elemY, handleScroll]);
+
+  const elementMounted = useCallback((e: HTMLDivElement) => {
+    // 마운트시 가장 위로 스크롤
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+    const array = Array.from(e.children);
+    const result: number[] = [];
+    // 랜딩 되었을 때 스크롤이 내려가 있을 경우 저장되는 값을 초기화
+    // ex) 중간 스크롤에서 새로고침
+    let temp = 0;
+    array.forEach((elem, idx) => {
+      const yAxis = elem.getBoundingClientRect().top;
+      if (idx === 0 && yAxis < 0) temp += Math.abs(yAxis);
+      result.push(elem.getBoundingClientRect().top + temp);
+    });
+    setElemY(result);
   }, []);
-  console.log("render");
+
   return (
-    <div className={styles.wrapper} ref={wrapperEl}>
+    <div className={styles.wrapper} ref={elementMounted}>
       <FirstBanner />
       <SecondBanner />
       <ThirdBanner />
-      <button
-        style={{ position: "fixed", top: 0 }}
-        onClick={() => window.scroll(0, 937)}
-      >
-        클릭
-      </button>
-      <button
-        style={{ position: "fixed", top: 0, left: 500 }}
-        onClick={() => window.scrollTo(0, 1874)}
-      >
-        클릭
-      </button>
     </div>
   );
 }
--- src/app/banners/FirstBanner.tsx
+++ src/app/banners/FirstBanner.tsx
@@ -5,12 +5,14 @@ import "./Banner.module.scss";
 
 export default function FirstBanner() {
   return (
-    //   <TypingAnimation
-    //     text={[
-    //       "안녕하세요, ChatGPT을 통해 YouTube 채널을 추천해드려요!",
-    //       "채널을 추천 드리기 위해 몇가지 질문을 준비했어요!",
-    //     ]}
-    //   />
-    <section>section 1</section>
+    <section>
+      <TypingAnimation
+        text={[
+          "안녕하세요, ChatGPT을 통해 YouTube 채널을 추천해드려요!",
+          "채널을 추천 드리기 위해 몇가지 질문을 준비했어요!",
+        ]}
+      />
+    </section>
+    // <section>section 1</section>
   );
 }
